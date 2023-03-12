5a3f00725419b581c448c22058aa5ccd0dbf6425
unknown <nataliematos12@gmail.com>
2023-03-12 04:36:04 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+function printHello(username) {
+    console.log('hello there' + username);
+}
+
+printHello('JD', 10);
+
+
+// var names = ['Nick', 'Megan', 'Abayomi', 'Ferny'];
+
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+
+// var num = 0;
+// num *= 7; // is the same as num +=1; + sets value equal to self plus incramental value
+
+// for (var index = 0; index < names.length; index ++) { //var count = 0; count < 10; count ++
+//     console.log(names[index]);
+// }
+
+
+
+
+
+
+// var str = 'Some string';
+// var num = 15;
+// var bool = true;
+// var jsGivens = null || undefined;
+// var notANumber = NaN;
+// var parsed = parseInt(str);
+
+
